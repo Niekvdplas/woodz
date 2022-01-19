0b65c2e14a0f0ad5c28b76cf0a53c66f8842a2e0
Niek van der Plas <niekvanderplas1@gmail.com>
2022-01-19 23:52:03 +0100

    another try

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -10,7 +10,7 @@ const About = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="lg:text-center">
           <h2
-            className={`text-base text-primary font-semibold tracking-wide uppercase`}
+            className={`mt-2 text-3xl leading-8 text-primary font-semibold tracking-tight uppercase sm:text-4xl`}
           >
             {title}
           </h2>
