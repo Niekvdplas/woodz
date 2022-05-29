785bc305d558ec483b2336ace2e290742b683184
Niek van der Plas <Niekvanderplas1@gmail.com>
2022-05-29 22:26:29 +0200

    Changed utisl

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -14,7 +14,7 @@ export type TimeLeft = {
 };
 
 export const calculateTimeLeft = () => {
-  const difference = +new Date(2022, 5, 6, 19, 0, 0, 0) - +new Date();
+  const difference = +new Date(2022, 5, 10, 19, 0, 0, 0) - +new Date();
 
   let timeLeft: TimeLeft = {} as TimeLeft;
 
