ac2d59cb0cc4dad93f7125060542467f7a780a36
Niek van der Plas <Niekvanderplas1@gmail.com>
2022-05-29 10:55:33 +0200

    Added under construction page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,14 +5,36 @@ import Head from 'next/head';
 import '../styles/main.css';
 
 function MyApp({ Component, pageProps }: any) {
+  const date = '06-10-2022';
+  const releaseDate = new Date(date);
+  const today = new Date();
   return (
     <>
       <Head>
         <link rel="shortcut icon" href="assets/images/logo2circlesmall.png" />
         <title>Woodz kozijnen</title>
       </Head>
-
-      <Component {...pageProps} />
+      {today.getTime() <= releaseDate.getTime() ? (
+        <div className="flex items-center justify-center h-screen bg-gray-200">
+          <div className="container">
+            <div className="bg-white rounded-lg shadow-lg p-5 md:p-20 mx-2">
+              <div className="text-center">
+                <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
+                  Woodz
+                  <span className="text-primary"> Kozijnen</span>
+                </h2>
+                <h3 className="text-xl md:text-3xl mt-10">Onder constructie</h3>
+                <p className="text-md md:text-xl mt-10">
+                  Vragen? mail met info@woodzkozijnen.nl of bel Bas- 06 53 94 22
+                  78
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <Component {...pageProps} />
+      )}
     </>
   );
 }
