a86ad182cac5391d864d16540386cb9922f59dca
Niek van der Plas <Niekvanderplas1@gmail.com>
2022-05-30 10:19:26 +0200

    Img ipv text

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,13 @@ import Head from 'next/head';
 
 import '../styles/main.css';
 import { Counter } from '../components/Timer';
+import config from '../config/index.json';
 
 function MyApp({ Component, pageProps }: any) {
   const releaseDate = new Date(2022, 5, 10, 19, 0, 0, 0);
   const today = new Date();
+  const { company } = config;
+  const { logo } = company;
   return (
     <>
       <Head>
@@ -19,14 +22,19 @@ function MyApp({ Component, pageProps }: any) {
           <div className="container">
             <div className="bg-white rounded-lg shadow-lg p-5 md:p-20 mx-2">
               <div className="text-center">
-                <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
-                  Woodz
-                  <span className="text-primary"> Kozijnen</span>
-                </h2>
+                <img
+                  alt="logo"
+                  src={logo}
+                  style={{
+                    display: 'block',
+                    marginLeft: 'auto',
+                    marginRight: 'auto',
+                    width: '50%',
+                  }}
+                />
                 <h3 className="text-xl md:text-3xl mt-10">Onder constructie</h3>
                 <p className="text-md md:text-xl mt-10">
-                  Vragen? mail met info@woodzkozijnen.nl of bel Bas- 06 53 94 22
-                  78
+                  Vragen? mail met info@woodzkozijnen.nl of bel Bas- 0653942278
                 </p>
               </div>
               <Counter />
