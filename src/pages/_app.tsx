ed3c7977f4f13e5c9d8120bc231f2db858e89c83
Niek van der Plas <Niekvanderplas1@gmail.com>
2022-06-01 23:43:19 +0200

    Test5

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,50 +7,47 @@ import { Counter } from '../components/Timer';
 import config from '../config/index.json';
 
 function MyApp({ Component, pageProps }: any) {
-  const releaseDate = new Date(2022, 5, 1, 23, 31, 0, 0);
+  const releaseDate = new Date(2022, 5, 1, 23, 47, 0, 0);
   const today = new Date();
   const { company } = config;
   const { logo } = company;
-  if (today.getTime() > releaseDate.getTime()) {
-    return (
-      <>
-        <Head>
-          <link rel="shortcut icon" href="assets/images/logo2circlesmall.png" />
-          <title>Woodz kozijnen</title>
-        </Head>
-
-        <Component {...pageProps} />
-      </>
-    );
-  }
   return (
     <>
       <Head>
         <link rel="shortcut icon" href="assets/images/logo2circlesmall.png" />
         <title>Woodz kozijnen</title>
       </Head>
-      <div className="flex items-center justify-center h-screen bg-gray-200">
-        <div className="container">
-          <div className="bg-white rounded-lg shadow-lg p-5 md:p-20 mx-2">
-            <div className="text-center">
-              <img
-                alt="logo"
-                src={logo}
-                style={{
-                  display: 'block',
-                  marginLeft: 'auto',
-                  marginRight: 'auto',
-                  width: '50%',
-                }}
-              />
-              <h3 className="text-xl md:text-3xl mt-10">Onder constructie</h3>
-              <p className="text-md md:text-xl mt-10">
-                Vragen? mail met info@woodzkozijnen.nl of bel Bas- 0653942278
-              </p>
+      <div>
+        {today.getTime() > releaseDate.getTime() ? (
+          <Component {...pageProps} />
+        ) : (
+          <div className="flex items-center justify-center h-screen bg-gray-200">
+            <div className="container">
+              <div className="bg-white rounded-lg shadow-lg p-5 md:p-20 mx-2">
+                <div className="text-center">
+                  <img
+                    alt="logo"
+                    src={logo}
+                    style={{
+                      display: 'block',
+                      marginLeft: 'auto',
+                      marginRight: 'auto',
+                      width: '50%',
+                    }}
+                  />
+                  <h3 className="text-xl md:text-3xl mt-10">
+                    Onder constructie
+                  </h3>
+                  <p className="text-md md:text-xl mt-10">
+                    Vragen? mail met info@woodzkozijnen.nl of bel Bas-
+                    0653942278
+                  </p>
+                </div>
+                <Counter />
+              </div>
             </div>
-            <Counter />
           </div>
-        </div>
+        )}
       </div>
     </>
   );
