59a0dd167576bf5b336d06113c61c1a429b145f5
Niek van der Plas <niekvanderplas1@gmail.com>
2023-06-21 09:54:54 +0200

    Changed nav icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@
 import React, { Fragment } from 'react';
 
 import { Popover, Transition } from '@headlessui/react';
-import { DocumentDuplicateIcon, XCircleIcon } from '@heroicons/react/24/outline';
+import { Bars3Icon, DocumentDuplicateIcon, XCircleIcon } from '@heroicons/react/24/outline';
 import { Link } from 'react-scroll';
 import imageUrlBuilder from "@sanity/image-url";
 
@@ -71,7 +71,7 @@ const Menu = ({logo} : Props) => {
                     className={`bg-background rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-secondary`}
                   >
                     <span className="sr-only">Open main menu</span>
-                    <DocumentDuplicateIcon className="h-6 w-6" aria-hidden="true" />
+                    <Bars3Icon className="h-6 w-6" aria-hidden="true" />
                   </Popover.Button>
                 </div>
               </div>
